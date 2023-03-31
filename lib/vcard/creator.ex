53404162616156fc7ad337951ca6136571252f39
Dimitrije <dimitrijedimitrijevic93@gmail.com>
2023-03-31 10:56:52 +0200

    additional properties

--- lib/vcard/creator.ex
+++ lib/vcard/creator.ex
@@ -3,6 +3,31 @@ defmodule Naiveical.Vcard.Creator do
   Module for creating Vcard(vcf) text files.
   """
 
+  ### This VCARD is generated trought the Thunderbird UI
+  ### Which I took as an inspiration for the fields.
+  # VCARD Example
+  # BEGIN:VCARD
+  # VERSION:3.0
+  # PRODID:-//Sabre//Sabre VObject 4.3.0//EN
+  # N:Name;Surname ;;;
+  # FN:Surname  Name
+  # NICKNAME:Nickkontakt
+  # EMAIL;TYPE=PREF,work;PREF=1:work@klasik.com
+  # TEL;TYPE=work:060111111111
+  # NOTE:Some notes
+  # URL;TYPE=home:https://klasik.com
+  # TITLE:Ceo
+  # ROLE:Ceo
+  # ORG:Company;Department
+  # BDAY;VALUE=DATE:19950324
+  # UID:688819b9-75ca-42ea-a517-b6ba205274d8
+  # URL;TYPE=work:https://klasik.com
+  # URL;TYPE=work:https://klasik.com
+  # URL;TYPE=work:https://klasik.com
+  # ADR;TYPE=work:;;Address 1;City;State/Province;11000;Serbia
+  # ADR:;;Address 2;City 2;State 2;159999;Serbia
+  # END:VCARD
+
   ### SOME BASIC FIELDS TO SUPPORT FOR START ###
 
   # FN consist of PREFIX, FIRSTNAME, MIDDLE NAME, SURNAME, SUFFIX
@@ -63,7 +88,7 @@ defmodule Naiveical.Vcard.Creator do
 
   @date_format "{YYYY}{0M}{0D}"
   @vcard_version "3.0"
-
+  @prod_id "-//Migadu-Excalt//"
   @doc """
   Create a simple VCard file.
   VERSION and FN property must be included in vcard object.
@@ -73,61 +98,84 @@ defmodule Naiveical.Vcard.Creator do
   @spec create_vcard(opts :: Keyword.t()) :: String.t()
   def create_vcard(opts \\ []) do
     uid = UUID.uuid4()
-    full_name = Keyword.get(opts, :fn)
-    emails = extract_additional_properties(opts, :email)
-    tel = extract_additional_properties(opts, :tel)
-    addresses = extract_additional_properties(opts, :adrs)
-    nickname = Keyword.get(opts, :nickname)
+    full_name = Keyword.get(opts, :fn, "")
+    emails = get_additional_properties(opts, :email)
+    tel = get_additional_properties(opts, :tel)
+    addresses = get_additional_properties(opts, :addrs)
+    nickname = Keyword.get(opts, :nickname, "")
+    title = Keyword.get(opts, :title, "")
+    role = Keyword.get(opts, :role, "")
+    org = Keyword.get(opts, :org, "")
+    websites = get_additional_properties(opts, :websites)
+    bday = get_bday(opts)
+    kind = Keyword.get(opts, :kind, "")
 
     ("""
-      BEGIN:VCARD
-      VERSION:#{@vcard_version}
-      UID:#{uid}
-      FN:#{full_name}
+     BEGIN:VCARD
+     VERSION:#{@vcard_version}
+     PRODID:#{@prod_id}
+     UID:#{uid}
+     FN:#{full_name}
      """ <>
        nickname <>
        emails <>
        tel <>
        addresses <>
+       title <>
+       role <>
+       org <>
+       websites <>
+       bday <>
+       kind <>
        """
        END:VCARD
        """)
     |> String.replace(~r/\r?\n/, "\r\n")
   end
 
-  def extract_additional_properties(opts, property, additional_properties \\ "")
+  def get_additional_properties(opts, property, default \\ "")
 
-  def extract_additional_properties(opts, :email, additional_properties) do
+  def get_additional_properties(opts, :email, default) do
     if Keyword.has_key?(opts, :email) do
       Keyword.get_values(opts, :email)
-      |> extract_emails()
+      |> get_emails()
     else
-      additional_properties
+      default
     end
   end
 
-  def extract_additional_properties(opts, :tel, additional_properties) do
+  def get_additional_properties(opts, :tel, default) do
     if Keyword.has_key?(opts, :tel) do
       opts
       |> Keyword.get_values(:tel)
-      |> extract_telephones()
+      |> get_telephones()
     else
-      additional_properties
+      default
     end
   end
 
-  def extract_additional_properties(opts, :adrs, additional_properties) do
-    if Keyword.has_key?(opts, :adrs) do
+  def get_additional_properties(opts, :addrs, default) do
+    if Keyword.has_key?(opts, :addrs) do
       opts
-      |> Keyword.get_values(:adrs)
-      |> extract_address()
+      |> Keyword.get_values(:addrs)
+      |> get_addresses
     else
-      additional_properties
+      default
+    end
+  end
+
+  def get_additional_properties(opts, :websites, default) do
+    if Keyword.has_key?(opts, :websites) do
+      opts
+      |> Keyword.get_values(:websites)
+      |> get_websites
+    else
+      default
     end
   end
 
   # Expect a list of values, [%{type: "type", value: val}]
-  defp extract_emails(emails) do
+  defp get_emails(emails) do
     emails
     |> Enum.map_join(fn %{type: type, value: val} ->
       # responsibility for validations of the user input should be on the client side.
@@ -139,7 +187,7 @@ defmodule Naiveical.Vcard.Creator do
     end)
   end
 
-  defp extract_telephones(tel) do
+  defp get_telephones(tel) do
     tel
     |> Enum.map_join(fn %{type: type, value: val} ->
       if is_nil(type) do
@@ -150,26 +198,60 @@ defmodule Naiveical.Vcard.Creator do
     end)
   end
 
-  defp extract_addresses(addresses) do
-    addreses
-    |> Enum.map_join(fn %{type: type, street: street, locality: locality, region: region, zip_code: zip_code, country: country} = c ->
+  defp get_websites(websites) do
+    websites
+    |> Enum.map_join(fn %{type: type, value: val} ->
+      if is_nil(type) do
+        "URL:#{val}"
+      else
+        "URL;TYPE=#{type}:#{val}"
+      end
+    end)
+  end
+
+  defp get_bday(opts) do
+    value = Keyword.get(opts, :bday, nil)
+
+    if is_nil(value) do
+      ""
+    else
+      value = Timex.format!(value, @date_format)
+      "BDAY;VALUE=DATE:#{value}"
+    end
+  end
+
+  defp get_addresses(addresses) do
+    addresses
+    |> Enum.map_join(fn %{
+                          type: type
+                        } = c ->
       comp = make_single_address_comp(c)
+
       if is_nil(type) do
         "ADR:;;#{comp}"
       else
-
+        "ADR;TYPE=#{type};;#{comp}"
       end
-     end)
+    end)
   end
 
-# %{type: type, street: street, locality: locality, region: region, zip_code: zip_code, country: country}
+  #  %{type: type, street: street, city: city, region: region, zip_code: zip_code, country: country}
   defp make_single_address_comp(c) do
-    comp = ""
-    comp = if is_nil(c.street), do: ";", else:
-   end
+    street = Map.get(c, :street, nil)
+    city = Map.get(c, :city, nil)
+    region = Map.get(c, :region, nil)
+    zip_code = Map.get(c, :zip_code, nil)
+    country = Map.get(c, :country, nil)
+
+    [street, city, region, zip_code, country]
+    |> Enum.map_join(fn c ->
+      if is_nil(c) do
+        ";"
+      else
+        "#{c};"
+      end
+    end)
+  end
 
   # By rfc first two components in ADR propery are ommited.
- end
-
-defp extract_websites(opts) do
 end
